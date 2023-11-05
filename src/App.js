0cc80e644acda58f87b9c1e8b590da4590d5ce65
rashedevs <rasheduap2015@gmail.com>
2023-11-05 15:43:47 +0600

    add draggable image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import Header from "./components/Header/Header";
 import Gallary from "./components/Gallary/Gallary";
 function App() {
   return (
     <div className="App">
-      <Header />
       <Gallary />
     </div>
   );
---
+++ src/components/DraggableImage/DraggableImage.js
@@ -0,0 +1,56 @@
+import { useDrag, useDrop } from "react-dnd";
+import React, { useState } from "react";
+import "./DraggableImage.css";
+
+const DraggableImage = ({ imgPath, index, onDrop, onCheckboxClick }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+  const ItemType = "IMAGE";
+
+  const [{ isDragging }, ref] = useDrag({
+    type: ItemType,
+    item: { index },
+  });
+
+  const handleHover = (hovered) => {
+    setIsHovered(hovered);
+  };
+
+  const [, drop] = useDrop({
+    accept: ItemType,
+    drop: (draggedItem) => onDrop(draggedItem.index, index),
+  });
+
+  const handleCheckboxClicked = () => {
+    setIsSelected(!isSelected);
+    onCheckboxClick(!isSelected);
+  };
+
+  return (
+    <div
+      onMouseEnter={() => handleHover(true)}
+      onMouseLeave={() => handleHover(false)}
+      ref={(node) => ref(drop(node))}
+      className={`grid-item${index === 0 ? " wide" : ""} ${
+        isSelected ? "checked-true" : ""
+      }`}
+    >
+      <img
+        src={imgPath}
+        alt={`pic ${index + 1} ${isDragging ? "dragging" : ""}`}
+      />
+
+      {(isHovered || isSelected) && (
+        <div className="checkbox-container">
+          <input
+            type="checkbox"
+            checked={isSelected}
+            onChange={handleCheckboxClicked}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DraggableImage;
