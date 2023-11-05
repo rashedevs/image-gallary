9db6cc521f58be64bb9e6e6ae7919f015cbe73f9
rashedevs <rasheduap2015@gmail.com>
2023-11-05 15:49:22 +0600

    header component modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,41 @@
 import React from "react";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ value, checked }) => {
+  const handleDeleteClick = () => {
+    if (checked) {
+      const elements = document.getElementsByClassName("checked-true");
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+      }
+    }
+  };
+
   return (
-    <div className="header">
-      <h2>Image Gallary</h2>
-      <h4>Delete selected images</h4>
-    </div>
+    <header className="app-header">
+      <div className="header-left">
+        <div></div>
+        <div>
+          {value > 0 ? (
+            <React.Fragment>
+              <input type="checkbox" checked />
+              <span>{`${value} Files Selected`}</span>
+            </React.Fragment>
+          ) : (
+            <span>Gallery</span>
+          )}
+        </div>
+      </div>
+      <div className="header-right">
+        {value > 0 ? (
+          <React.Fragment>
+            <span onClick={handleDeleteClick}>Delete Files</span>
+          </React.Fragment>
+        ) : (
+          <span></span>
+        )}
+      </div>
+    </header>
   );
 };
 
